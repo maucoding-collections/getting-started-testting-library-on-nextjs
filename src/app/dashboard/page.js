625f0e689d39355ab2aaed8a87f0e8e0198098f0
yussan <main.yussan@gmail.com>
2024-01-13 21:14:40 +0700

    feat(login logic): ready to use

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,24 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 const DashboardPage = () => {
-  return <>this is dashboard page</>;
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "lightgray",
+      }}
+    >
+      <Typography as="h1" component="h1">
+        This is Dashboard&nbsp;
+        <small>Go go get ready!!!</small>
+      </Typography>
+    </Box>
+  );
 };
 
 export default DashboardPage;
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import FormGroup from "@mui/material/FormGroup";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormLabel from "@mui/material/FormLabel";
 import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+
+const RESPONSE = {
+  SUCCESS: {
+    status: 200,
+    message: "Login Success",
+  },
+  ERROR: {
+    status: 400,
+    message: "Login Failed",
+  },
+};
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState({});
+
+  const Router = useRouter();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    if (username === "password" && password === "password") {
+      setResponse(RESPONSE.SUCCESS);
+      setTimeout(() => {
+        Router.push("/dashboard");
+      }, 1500);
+    } else {
+      setResponse(RESPONSE.ERROR);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -25,7 +59,7 @@ const LoginPage = () => {
           border: "1px solid lightgray",
         }}
       >
-        <form method="post">
+        <form method="post" action="#" onSubmit={submitHandler}>
           <Typography
             variant="h6"
             component="h1"
@@ -36,18 +70,47 @@ const LoginPage = () => {
           <Divider sx={{ marginBottom: "20px" }} />
           <FormGroup sx={{ marginBottom: "20px" }}>
             <FormLabel>Username</FormLabel>
-            <Input type="text" />
-            <FormHelperText>Type username here</FormHelperText>
+            <Input
+              type="text"
+              placeholder="Type username here"
+              value={username}
+              onChange={(e) => {
+                e.preventDefault();
+                setUsername(e.target.value);
+              }}
+            />
           </FormGroup>
 
-          <FormGroup sx={{ marginBottom: "20px" }}>
+          <FormGroup sx={{ marginBottom: "30px" }}>
             <FormLabel>Password</FormLabel>
-            <Input type="password" />
-            <FormHelperText>Type password here</FormHelperText>
+            <Input
+              type="password"
+              placeholder="Type password here"
+              onChange={(e) => {
+                e.preventDefault();
+                setPassword(e.target.value);
+              }}
+            />
           </FormGroup>
+
+          {response.status && (
+            <Alert
+              severity={
+                response.status === RESPONSE.SUCCESS.status
+                  ? "success"
+                  : "error"
+              }
+            >
+              {response.message}
+            </Alert>
+          )}
 
           <Box sx={{ display: "block", margin: "10px", textAlign: "center" }}>
-            <Button variant="contained" type="submit">
+            <Button
+              disabled={response.status === RESPONSE.SUCCESS.status}
+              variant="contained"
+              type="submit"
+            >
               Login
             </Button>
           </Box>
