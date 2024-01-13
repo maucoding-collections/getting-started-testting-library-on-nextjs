20512993f477cbe6ac3c85bbd080888d6c5b96ce
yussan <main.yussan@gmail.com>
2024-01-13 20:21:46 +0700

    feat(login static): static login is ready

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <>this is dashboard page</>;
+};
+
+export default DashboardPage;
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,60 @@
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import FormGroup from "@mui/material/FormGroup";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+
 const LoginPage = () => {
-  return <>login page</>;
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          padding: "15px",
+          width: "300px",
+          border: "1px solid lightgray",
+        }}
+      >
+        <form method="post">
+          <Typography
+            variant="h6"
+            component="h1"
+            sx={{ paddingBottom: "15px", textAlign: "center" }}
+          >
+            Login
+          </Typography>
+          <Divider sx={{ marginBottom: "20px" }} />
+          <FormGroup sx={{ marginBottom: "20px" }}>
+            <FormLabel>Username</FormLabel>
+            <Input type="text" />
+            <FormHelperText>Type username here</FormHelperText>
+          </FormGroup>
+
+          <FormGroup sx={{ marginBottom: "20px" }}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" />
+            <FormHelperText>Type password here</FormHelperText>
+          </FormGroup>
+
+          <Box sx={{ display: "block", margin: "10px", textAlign: "center" }}>
+            <Button variant="contained" type="submit">
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
